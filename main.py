771919b348243c7bbe7f33b3d66a30d9eb8aa6f3
Asha <ashabjohnson@gmail.com>
2019-07-28 16:37:08 -0700

    fixed bug

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 
 
-#
-# app = webapp2.WSGIApplication([
-#     ('/', Page)
-# ], debug=True)
+# Step 2: Set up Jinja Environment
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+)
+
+class AddCollege (webapp2.RequestHandler):
+        def get(self):
+            template = jinja_env.get_template('AddCollege.html')
+            self.response.write(template.render())
+
+        def post(self):
+            template_vars = {
+                "college_name": self.request.get("college_name"),
+                "housing": self.request.get("housing"),
+                "travel": self.request.get("travel"),
+                "tuition": self.request.get("tuition"),
+            }
+            template = jinja_env.get_template('MainPage.html')
+            self.response.write(template.render(template_vars))
+
+app = webapp2.WSGIApplication([
+    ('/', AddCollege),
+], debug=True)
