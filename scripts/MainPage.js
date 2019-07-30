88debd4a714d8d18109f3af60a97c35f06499d27
Dalen Witthoft <dalen2020@gmail.com>
2019-07-30 14:52:21 -0700

    The javascript adds the color changes and there are some new css tags regarding budget it accesses.

--- scripts/MainPage.js
+++ scripts/MainPage.js
@@ -1,1 +1,12 @@
-let total_comparison = document.querySelectorAll("dataset['total']");
+let total_elements = document.querySelectorAll(".total");
+let budget = 1000;
+for(let total_element of total_elements){
+  let int_total = parseInt(total_element.dataset['total']);
+  if(int_total < budget){
+      //make green
+      total_element.classList.add("isInBudget");
+  }
+  else{
+    total_element.classList.add("isNotInBudget");
+  }
+}
