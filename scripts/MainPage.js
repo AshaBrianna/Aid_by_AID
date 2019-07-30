44b99444f924151dacce8996c9ac64ad6c60bc92
Dalen Witthoft <dalen2020@gmail.com>
2019-07-30 16:44:54 -0700

    this new js may not work rn but it will soon :)

--- scripts/MainPage.js
+++ scripts/MainPage.js
@@ -1,5 +1,5 @@
 let total_elements = document.querySelectorAll(".total");
-let budget = ;
+let budget = document.querySelctorAll("");
 for(let total_element of total_elements){
   let int_total = parseInt(total_element.dataset['total']);
   if(int_total < budget){
@@ -7,12 +7,9 @@ for(let total_element of total_elements){
       total_element.classList.add("isInBudget");
   }
   else{
-    //make red
     total_element.classList.add("isNotInBudget");
   }
 }
-
-// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
 $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
