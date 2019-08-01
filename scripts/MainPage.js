a858833e743e6d0b31e6be5c0a8c050d10789db3
Dalen Witthoft <dalen2020@gmail.com>
2019-08-01 14:31:07 -0700

    Fixing totals

--- scripts/MainPage.js
+++ scripts/MainPage.js
@@ -1,11 +1,11 @@
 
 let total_elements = document.querySelectorAll(".total");
-let budget=globalBudget;
+let budget = globalBudget;
 let grants = globalGrants;
 
 for(let total_element of total_elements){
   let int_total = parseInt(total_element.dataset['total']);
-  int_total -= grants;
+  // int_total -= grants;
   if(int_total < budget){
       //make green
       total_element.classList.add("isInBudget");
